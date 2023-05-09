da2a939df6ba1bccbc5b459c592a27be5a1d0b2b
Ondrej Koprajda <ondrej.koprajda@gmail.com>
2023-05-09 23:08:51 +0200

    Vyreseni casti 1,2,3,4,5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.math.BigDecimal;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-//    public static void ukol1() {
-//        System.out.println(Hello world!)
-//    }
+    public static void ukol1() {
+        System.out.println("Hello world");
+    }
     public static void ukol2() {
        String uzivatel = "Novak Petr";
        System.out.println(uzivatel);
@@ -16,25 +18,25 @@ public class Main {
        System.out.println(pocetLekci);
     }
 //
-//    public static void ukol4() {
-//        double cena = 10,0;
-//        System.out.println(cena);
-//    }
+    public static void ukol4() {
+        double cena = 10.0;
+        // misto carky musi byt tecka, ale nechce mi to promitnout :-(
+        System.out.println(cena);
+    }
 //
-//    public static void ukol5() {
-//        double cena = 0; // Nastavíme do proměnné hodnotu 0
-//        // Desetkrát přičteme hodnotu jedna desetina (0,1):
-//        for (int i = 0; i < 10; i++) {
-//            cena += 0.1;
-//               // operátor "+=" znamená: přičti ke stávající
-//               // hodnotě navíc 0.1
-//        }
-//        System.out.println(cena);
-//        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
-//    }
-//
-//    public static void ukol6() {
-//        BigDecimal cena = 0;
+    public static void ukol5() {
+        BigDecimal cena = 0; // Nastavíme do proměnné hodnotu 0
+        // Desetkrát přičteme hodnotu jedna desetina (0,1):
+        for (int i = 0; i < 10; i++); {
+            cena += 0.1;
+               // operátor "+=" znamená: přičti ke stávající
+               // hodnotě navíc 0.1
+        }
+        System.out.println(cena);
+        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
+
+    public static void ukol6() {
+        BigDecimal cena = 0;
 //        cena = cena.add(BigDecimal.valueOf(0.1));
 //        System.out.println(cena);
 //    }
@@ -49,23 +51,25 @@ public class Main {
 //        System.out.println("Velikost košile je: ", velikostKosile, ".");
 //    }
 //
-//    public void ukol9() {
-//        System.out.println("Metoda *skoro* bez chybičky!");
-//    }
+        public static void ukol9() {
+            System.out.println("Metoda *skoro* bez chybičky!");
+        }
 
-    public static void main(String[] args) {
-        System.out.println("Oprav chyby a spusť všechny metody!");
-        System.out.println("Komentáře odstraníš klávesovou "
-                +"zkratkou <Ctrl>+</> - použij lomítko "
-                +"na numerické klávesnici.");
-//        ukol1();
-       ukol2();
-        ukol3();
-//        ukol4();
-//        ukol5();
+        public static void main (String[]args){
+            System.out.println("Oprav chyby a spusť všechny metody!");
+            System.out.println("Komentáře odstraníš klávesovou "
+                    + "zkratkou <Ctrl>+</> - použij lomítko "
+                    + "na numerické klávesnici.");
+            ukol1();
+            ukol2();
+            ukol3();
+            ukol4();
+            ukol5();
 //        ukol6();
 //        ukol7();
 //        ukol8();
-//        ukol9();
+            ukol9();
+        }
     }
 }
+
